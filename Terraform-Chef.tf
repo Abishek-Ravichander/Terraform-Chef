b45c59658d69b6fb3e1f1f2ea111bdf851383077
Abishek-Ravichander <70654020+Abishek-Ravichander@users.noreply.github.com>
2021-08-03 13:22:31 +0530

    Update Terraform-Chef.tf

--- Terraform-Chef.tf
+++ Terraform-Chef.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "web1" {
    ami           = "ami-03295ec1641924349"
    instance_type = "t2.micro"
    count = 1
-   vpc_security_group_ids = [sg-0990c12803c100850]
+   vpc_security_group_ids = ["sg-0990c12803c100850"]
  }
 

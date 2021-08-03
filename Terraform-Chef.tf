dab3ac05d8561d44e267600217b7e7ea724f79b7
Abishek-Ravichander <70654020+Abishek-Ravichander@users.noreply.github.com>
2021-08-03 13:20:25 +0530

    Update Terraform-Chef.tf

--- Terraform-Chef.tf
+++ Terraform-Chef.tf
@@ -2,40 +2,6 @@ resource "aws_instance" "web1" {
    ami           = "ami-03295ec1641924349"
    instance_type = "t2.micro"
    count = 1
-   vpc_security_group_ids = [aws_security_group.Terraform-Chef.id]
+   vpc_security_group_ids = [sg-0990c12803c100850]
  }
 
-#resource "aws_vpc" "default" {
- # cidr_block = "172.31.0.0/16"
-#}
-
-
-resource "aws_security_group" "Terraform-Chef" {
-  name        = "Terraform-Chef"
-  description = "Used in the terraform"
-  vpc_id      = "vpc-b2d8bfcf"
-
-  # SSH access from anywhere
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # HTTP access from the VPC
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
-  }
-
-  # outbound internet access
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}

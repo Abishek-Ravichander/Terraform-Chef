cac775d70c1b63a048bb8113b3f5aa1c386a7c63
Abishek-Ravichander <70654020+Abishek-Ravichander@users.noreply.github.com>
2021-08-03 21:08:44 +0530

    Update Terraform-Chef.tf

--- Terraform-Chef.tf
+++ Terraform-Chef.tf
@@ -4,5 +4,16 @@ resource "aws_instance" "web1" {
    count = 1
    vpc_security_group_ids = ["sg-0990c12803c100850"]
    key_name               = "Linux_Terraform-Chef"
+   
+   provisioner "remote-exec" {
+    inline = [
+      "wget -O /tmp/chef.rpm https://MYSERVER/chef_installers/chef-15.8.23-1.el7.x86_64.rpm",
+      "rpm -Uvh /tmp/chef.rpm",
+      "wget -O /tmp/base.tgz https://MYSERVER/policyfiles/base.tgz",
+      "tar -C /tmp -xzf /tmp/base.tgz",
+      "PWD=/tmp/base chef-client -z",
+    ]
+  }
+   
  }
 

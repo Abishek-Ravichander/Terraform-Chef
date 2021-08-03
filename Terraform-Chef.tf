e255927bb4ce05be1a79ad1c2dbfe031ba3636b3
Abishek-Ravichander <70654020+Abishek-Ravichander@users.noreply.github.com>
2021-08-03 19:02:25 +0530

    Update Terraform-Chef.tf

--- Terraform-Chef.tf
+++ Terraform-Chef.tf
@@ -5,3 +5,13 @@ resource "aws_instance" "web1" {
    vpc_security_group_ids = ["sg-0990c12803c100850"]
  }
 
+resource "tls_private_key" "this" {
+  algorithm = "RSA"
+}
+
+module "key_pair" {
+  source = "terraform-aws-modules/key-pair/aws"
+
+  key_name   = "deployer-one"
+  public_key = tls_private_key.this.public_key_openssh
+}

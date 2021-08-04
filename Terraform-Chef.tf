8200e27942cdae733563a63de4dfaa8608e72e4d
Abishek-Ravichander <70654020+Abishek-Ravichander@users.noreply.github.com>
2021-08-04 11:14:19 +0530

    Update Terraform-Chef.tf

--- Terraform-Chef.tf
+++ Terraform-Chef.tf
@@ -10,7 +10,8 @@ provisioner "remote-exec" {
     inline = [    
        "wget -O /tmp/chef.rpm https://packages.chef.io/files/stable/chef-workstation/20.7.96/el/7/chef-workstation-20.7.96-1.el7.x86_64.rpm",
        "curl -L https://omnitruck.chef.io/install.sh | sudo bash -s -- -v 15.8.23",
-       "chef-client -v",       
+       "chef-client -v",   
+       "which chef",
     ]
       
       connection {

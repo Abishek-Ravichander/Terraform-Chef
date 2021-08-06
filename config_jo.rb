6e3b6beff7e69af66d16d9b345ec5e85aff06e45
Abishek-Ravichander <70654020+Abishek-Ravichander@users.noreply.github.com>
2021-08-06 18:18:24 +0530

    Add files via upload

---
+++ config_jo.rb
@@ -0,0 +1,9 @@
+# See http://docs.chef.io/workstation/config_rb/ for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "abishekkr"
+client_key               "#{current_dir}/abishekkr.pem"
+chef_server_url          "https://api.chef.io/organizations/jocg"
+cookbook_path            ["#{current_dir}/../cookbooks"]

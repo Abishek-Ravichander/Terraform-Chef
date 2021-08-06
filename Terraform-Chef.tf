1001aff6bc28078944a8f3c3871c597fa5247fc1
Abishek-Ravichander <70654020+Abishek-Ravichander@users.noreply.github.com>
2021-08-06 15:18:57 +0530

    Update Terraform-Chef.tf

--- Terraform-Chef.tf
+++ Terraform-Chef.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web1" {
    ami           = "ami-0c2b8ca1dad447f8a"
    instance_type = "t2.micro"
-   count = 1
+ #  count = 1
    vpc_security_group_ids = ["sg-0990c12803c100850"]
    key_name               = "Linux_Terraform-Chef"
    subnet_id       = "subnet-a24fd693"
